b6e050ca74ab242224bf0304b82330a3bf8dd466
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-06-22 02:53:07 +0530

    Passing dishes details as props to MenuComponent

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import react, { Component} from 'react';
 
-function App() {
-  return (
-    <div>
-      {/* add navbar */}
-      <Navbar dark color="primary">
-        <div className="container">
-          {/* Brand name added */}
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      {/* using menu component from MenuComponent */}
-      <Menu />
-    </div>
-  );
+// changing from function to class component
+// function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+  render() {
+    return (
+      <div>
+        {/* add navbar */}
+        < Navbar dark color="primary" >
+          <div className="container">
+            {/* Brand name added */}
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar >
+        {/* using menu component from MenuComponent */}
+        {/* passing the dishes details as props */}
+        < Menu dishes={this.state.dishes} />
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,48 +6,8 @@ class Menu extends Component {
     constructor(props) {
         // supplying this props to the super class
         super(props);
-
         // state stores the properties of the component, so we can make us of
         this.state = {
-            // js object to save the list of dishes(objects)
-            dishes: [
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ]
         }
     }
 
@@ -55,7 +15,8 @@ class Menu extends Component {
     render() {
         // map will iterate over items over array
         // map them into list of items here
-        const menu = this.state.dishes.map((dish) => {
+        // now receiving dishes details from props
+        const menu = this.props.dishes.map((dish) => {
             return (
                 // here we will create the view for each of the item dishes
                 // in js every item requires a key attribute to be specified
