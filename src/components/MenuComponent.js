8ce53b03eb546e7ba7610faa6b2aeb9ba1c1ff8b
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-06-24 00:15:20 +0530

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from "../components/DishdetailComponent";
 
 class Menu extends Component {
     // defining constructor
@@ -20,17 +20,11 @@ class Menu extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle heading>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <DishDetail dish={dish} />
             );
         }
         else {
-            // if nothinf is selected then just pass div
+            // if nothing is selected then just pass div
             return (
                 <div></div>
             );
