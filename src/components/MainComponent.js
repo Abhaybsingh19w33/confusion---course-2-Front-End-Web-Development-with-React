d3fa010f8e312cc68ece4f179fe4908d3cdde617
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-06-25 14:11:49 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import Menu from './/MenuComponent';
 import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import React, { Component } from 'react';
 import DishDetail from "./DishdetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 // changing from function to class component
 // function App() {
@@ -15,6 +19,9 @@ class Main extends Component {
         this.state = {
             dishes: DISHES,
             // selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -25,7 +32,12 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                // displaying only dish which are featured
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         return (
@@ -37,6 +49,7 @@ class Main extends Component {
                     {/* this Route will not allow us to pasa ny props into menu component */}
                     {/* so we have to pass the props as an function component */}
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     {/* if no path matches then redirect to home page */}
                     <Redirect to="/home" />
                 </Switch>
